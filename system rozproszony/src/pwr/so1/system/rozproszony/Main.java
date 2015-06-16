d8d8ae5b6e540552c2d47334dfdb624c04199918
ziomal52 <tom.jopek@gmail.com>
2015-06-16 02:12:42 +0200

    final version

--- system rozproszony/src/pwr/so1/system/rozproszony/Main.java
+++ system rozproszony/src/pwr/so1/system/rozproszony/Main.java
@@ -1,29 +1,55 @@
 package pwr.so1.system.rozproszony;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main
 {
 	
+	static int N;
+	static int p;
+	static int z;
+	static int r;
+	
 	public static void main(String[] args)
 	{
-		Symulacja symulacja = new Symulacja(70,20,80,5,10,70,15);
-		symulacja.start();
-		symulacja.zalozeniaSymulacji();
 		
-		symulacja.pierwszyAlgorytm();
-		symulacja.wypisz();
+		Scanner sc = null;
 		
-		symulacja.drugiAlgorytm();
-		symulacja.wypisz();
-		
-		symulacja.trzeciAlgorytm();
-		symulacja.wypisz();
-		
+		System.out.println("Podaj ilosc procesorow, prog, zakres, i minimalne obciazenie:");
+						
+		try
+		{
+			sc = new Scanner(System.in);
+			N = sc.nextInt();
+			p = sc.nextInt();
+			z = sc.nextInt();
+			r = sc.nextInt();
+			
+			Symulacja symulacja = new Symulacja(N,20,p,z,5,70,r);
+			
+			symulacja.start();
+			symulacja.zalozeniaSymulacji();
+			
+			symulacja.pierwszyAlgorytm();
+			symulacja.wypisz();
+			
+			symulacja.drugiAlgorytm();
+			symulacja.wypisz();
+			
+			symulacja.trzeciAlgorytm();
+			symulacja.wypisz();			
+			
+		}
+		catch(InputMismatchException f)
+		{
+			System.out.println("BLAD");
+		}
+		catch(IllegalArgumentException e)
+		{
+			System.out.println("BLAD");
+		}	
 		
 	}
-	
-	
-	
-	
-	
 
 }
